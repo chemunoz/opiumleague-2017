724525c8aacbd907678311857149f21c8016dc1d
Chema Muñoz <chemunoz78@gmail.com>
2018-12-16 00:20:40 +0100

    Update charts from Google to Highcharts and new KPIs

--- js/profile.js
+++ js/profile.js
@@ -18,94 +18,248 @@
         url_parameters[hash[0]] = hash[1];
       }
       // console.log(url_parameters);
-      console.log("Usuario elegido:", $scope.players_profile[url_parameters.id]);
+
+      $scope.player = $scope.players_profile[url_parameters.id];
+      console.log("Usuario elegido:", $scope.player);
+
+      $scope.player.score_max = Math.max.apply(null, $scope.player.points.filter((value)=>{return value !== null;}));
+      $scope.player.positions_general_differences = $scope.player.positions_general.map(function(value, index) {
+        return (index === 0) ? 0 : $scope.player.positions_general[index-1] - value;
+      } );
+      $scope.player.positions_general_differences_max = Math.max.apply(null, $scope.player.positions_general_differences.filter((value)=>{return !isNaN(value);}));
+      $scope.player.positions_general_differences_min = Math.abs(Math.min.apply(null, $scope.player.positions_general_differences.filter((value)=>{return !isNaN(value);})));
+
+      let ejes = {
+        XJornada: [],
+        YJornada: [],
+        YMedia: [],
+        YPosicionesGeneral: [],
+        YPosicionesJornada: []
+      };
+
+      for (let i = 0; i < $scope.player.points.filter((value)=>{return value !== null;}).length; i++) {
+        // if ((i+1) >= $scope.player.points.filter((value)=>{return value !== null;}).length - 5){
+          ejes.XJornada.push(`J${i+1}`);
+          ejes.YJornada.push($scope.player.points[i]);
+          ejes.YMedia.push($scope.tables[`jornada_${i+1}`].score_average);
+          ejes.YPosicionesGeneral.push($scope.player.positions_general[i]);
+          ejes.YPosicionesJornada.push($scope.player.positions_jornada[i]);
+        // }
+      }
+
+
+
+
 
 
 
       // CHARTS
-      let data_columns = [];
-      let data_columns2 = [];
-      data_columns.push(["Jornada", "Posición", { role: 'annotation' } ]);
-      data_columns2.push(["Jornada", "Puntos", "Media"]);
-
-      for (let i = 0; i < $scope.players_profile[url_parameters.id].points.filter((value)=>{return value !== null;}).length; i++) {
-        data_columns.push([`J${i+1}`, $scope.players_profile[url_parameters.id].positions_general[i], $scope.players_profile[url_parameters.id].positions_general[i]]);
-        data_columns2.push([`J${i+1}`, $scope.players_profile[url_parameters.id].points[i], $scope.tables[`jornada_${i+1}`].score_average]);
-      }
-      $scope.player = $scope.players_profile[url_parameters.id];
-
-      // let data_columns = [[]];
-      // data_columns[0].push([$scope.players_profile[url_parameters.id].team].concat($scope.players_profile[url_parameters.id].points.filter((value)=>{return value !== null;})));
-      console.log("points", data_columns);
-
-      // GRAFICO EVOLUCION
-      // let chart = c3.generate({
-      //   bindto: '#chart_profile',
-      //   data: {
-      //     columns: data_columns[0],
-      //     labels: true
-      //   },
-      //   size: {
-      //     height: 500
-      //   },
-      //   padding: {
-      //     top: 5,
-      //     bottom: 5,
-      //     right: 50,
-      //     left: 50
-      //   },
-      //   axis: {
-      //     x: {
-      //       type: 'category',
-      //       categories: ['J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9', 'J10', 'J11', 'J12', 'J13', 'J14', 'J15', 'J16', 'J17', 'J18', 'J19', 'J20', 'J21', 'J22', 'J23', 'J24', 'J25', 'J26', 'J27', 'J28', 'J29', 'J30', 'J31', 'J32', 'J33', 'J34', 'J35', 'J36', 'J37', 'J38']
-      //     }
-      //   }
-      // });
-
-      google.charts.load('current', {packages: ['corechart', 'line']});
-      google.charts.setOnLoadCallback(()=>{
-        let data = google.visualization.arrayToDataTable(
-          data_columns
-        );
-        let data2 = google.visualization.arrayToDataTable(
-          data_columns2
-        );
-
-        let options = {
-          title: `Posiciones por Jornada`,
-          // curveType: 'function',
-          legend: { position: 'bottom' },
-          pointSize: 5,
-          vAxis:{
-            direction: -1
+      Highcharts.chart('points_chart', {
+          chart: {
+              zoomType: 'xy'
           },
-          animation:{
-            startup: true,
-            duration: 2000
+          title: {
+              text: 'PUNTOS'
           },
-          height: 450
-        };
-
-        let options2 = {
-          title: `Puntos por Jornada`,
-          // curveType: 'function',
-          legend: { position: 'bottom' },
-          pointSize: 5,
-          animation:{
-            startup: true,
-            duration: 2000
+          subtitle: {
+              text: 'Jornada vs Media'
           },
-          height: 450,
-        };
-
-        let chart = new google.visualization.LineChart(document.getElementById('positions_chart'));
-        let chart2 = new google.visualization.LineChart(document.getElementById('points_chart'));
-
-        // Draw
-        chart.draw(data, options);
-        chart2.draw(data2, options2);
+          credits: {
+            enabled: false
+          },
+          plotOptions: {
+            series: {
+              dataLabels: {
+                format: '{y}',
+                enabled: true,
+                color: '#294469',
+                shadow: false,
+                // align: 'right',
+                // x: -25,
+                // style: {"fontSize": "10px", "textShadow": "0px" }
+              },
+              pointPadding: 0.1,
+              groupPadding: 0
+           }
+          },
+          xAxis: [{
+              categories: ejes.XJornada,
+              crosshair: true,
+              min: ejes.XJornada.length - 5,
+              max: ejes.XJornada.length - 1,
+              scrollbar: {
+                enabled: true
+              }
+          }],
+          yAxis: [
+            { // Primary yAxis
+              title: {
+                  text: null, // 'Media',
+                  style: {
+                      color: Highcharts.getOptions().colors[1]
+                  }
+              },
+              labels: {
+                  format: '{value}  pts',
+                  style: {
+                      color: Highcharts.getOptions().colors[1]
+                  }
+              }
+            },
+            { // Secondary yAxis
+              title: {
+                  text: null, // 'Jornada',
+                  style: {
+                      color: Highcharts.getOptions().colors[0]
+                  }
+              },
+              labels: {
+                  format: '{value} pts',
+                  style: {
+                      color: Highcharts.getOptions().colors[0]
+                  }
+              },
+              opposite: true
+            }
+          ],
+          tooltip: {
+              shared: true
+          },
+          // legend: {
+          //     layout: 'vertical',
+          //     align: 'left',
+          //     x: 120,
+          //     verticalAlign: 'top',
+          //     y: 100,
+          //     floating: true,
+          //     backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+          // },
+          series: [
+            {
+              name: 'Jornada',
+              type: 'column',
+              yAxis: 1,
+              data: ejes.YJornada,
+              tooltip: {
+                  valueSuffix: ' pts'
+              },
+              pointWidth: 40
+            },
+            {
+              name: 'Media',
+              type: 'spline',
+              data: ejes.YMedia,
+              tooltip: {
+                  valueSuffix: ' pts'
+              }
+          }]
       });
 
+
+
+
+      Highcharts.chart('positions_chart', {
+          chart: {
+              zoomType: 'xy'
+          },
+          title: {
+              text: 'POSICIONES'
+          },
+          subtitle: {
+              text: 'Jornada vs General'
+          },
+          credits: {
+            enabled: false
+          },
+          plotOptions: {
+            series: {
+              dataLabels: {
+                format: '{y}',
+                enabled: true,
+                color: '#294469',
+                shadow: false,
+                // align: 'right',
+                // x: -25,
+                // style: {"fontSize": "10px", "textShadow": "0px" }
+              },
+              pointPadding: 0.1,
+              groupPadding: 0
+           }
+          },
+          xAxis: [{
+              categories: ejes.XJornada,
+              crosshair: true,
+              min: ejes.XJornada.length - 5,
+              max: ejes.XJornada.length - 1,
+              scrollbar: {
+                enabled: true
+              }
+          }],
+          yAxis: [
+            { // Primary yAxis
+              title: {
+                  text: null, // 'General',
+                  style: {
+                      color: Highcharts.getOptions().colors[1]
+                  }
+              },
+              labels: {
+                  format: '{value}',
+                  style: {
+                      color: Highcharts.getOptions().colors[1]
+                  }
+              },
+              reversed: true,
+              tickInterval: 1
+            },
+            { // Secondary yAxis
+              title: {
+                  text: null, // 'Jornada',
+                  style: {
+                      color: Highcharts.getOptions().colors[0]
+                  }
+              },
+              labels: {
+                  format: '{value}',
+                  style: {
+                      color: Highcharts.getOptions().colors[0]
+                  }
+              },
+              opposite: true,
+              reversed: true,
+              tickInterval: 1
+            }
+          ],
+          tooltip: {
+              shared: true
+          },
+          // legend: {
+          //     layout: 'vertical',
+          //     align: 'left',
+          //     x: 120,
+          //     verticalAlign: 'top',
+          //     y: 100,
+          //     floating: true,
+          //     backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+          // },
+          series: [{
+              name: 'Jornada',
+              type: 'spline',
+              yAxis: 1,
+              data: ejes.YPosicionesJornada,
+              tooltip: {
+                  valueSuffix: 'º puesto'
+              }
+
+          }, {
+              name: 'General',
+              type: 'spline',
+              data: ejes.YPosicionesGeneral,
+              tooltip: {
+                  valueSuffix: 'º puesto'
+              }
+          }]
+      });
 
 
     });
